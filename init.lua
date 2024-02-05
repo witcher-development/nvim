15159beb4c5c232a8a4d4b9972dc9a598a6cb514
roman <roman@luchs-sec.com>
2024-02-05 13:21:55 +0100

    Go formatting

--- init.lua
+++ init.lua
@@ -283,7 +283,22 @@ require('lazy').setup({
     }
   },
  
-  { 'joerdav/templ.vim' }
+  { 'joerdav/templ.vim' },
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  }
 
 }, {})
 
@@ -708,3 +723,8 @@ lsp.setup()
 -- vim: ts=2 sts=2 sw=2 et
 vim.cmd('colorscheme rose-pine')
 
+vim.api.nvim_create_user_command('F',function()
+  pcall(function()
+    require("go.format").goimport()
+  end)
+end, {})
