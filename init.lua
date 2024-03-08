17db94549fd8a525a861691d7dfb34a0aacd6375
roman <roman@luchs-sec.com>
2024-03-08 13:43:11 +0100

    Better scope highlight

--- init.lua
+++ init.lua
@@ -360,6 +360,19 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnos
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
+require("ibl").setup({
+	indent = {
+		char = "▏", -- This is a slightly thinner char than the default one, check :help ibl.config.indent.char
+	},
+	scope = {
+		show_start = false,
+		show_end = false,
+	},
+})
+-- disable indentation on the first level
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
